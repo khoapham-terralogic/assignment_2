aabc2726e9defdf77f307c943c4f4dc39b596b4f
blackparadise0407 <blackparadise0407@gmail.com>
2020-07-16 22:07:38 +0700

    update store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import './assets/styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import store from './redux/store'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
